949a6d2572180b9544f4763019c5ff269ef57f5b
MrRen <543169072@qq.com>
2018-12-19 13:43:59 +0800

    init

---
+++ src/ApiResult.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: mrren
+ * Date: 2018/11/30
+ * Time: 1:38 PM
+ */
+class ApiResult
+{
+
+    private $data;
+
+
+    public function __construct($stringOrdata)
+    {
+        if (is_array($stringOrdata))
+            $this->data = $stringOrdata;
+        else
+            $this->data = json_decode($stringOrdata, true);
+    }
+
+    public function isSuccess($code_value = 1)
+    {
+        return $this->data["code"] == $code_value;
+    }
+
+    public function getMsg()
+    {
+        return $this->data["msg"];
+    }
+
+    public function getCode()
+    {
+        return $this->data["code"];
+    }
+
+    public function getData()
+    {
+
+        return $this->data["data"];
+    }
+
+    /*
+     *
+     */
+    public function getApiResultData($key)
+    {
+
+        return new ApiResult($this->getData()[$key]);
+    }
+
+}
